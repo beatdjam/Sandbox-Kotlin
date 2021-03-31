e947946ed7edbd600c1b5a010b67d2fdb630b42b
T.Hori <beat3526@gmail.com>
2021-03-31 23:48:41 +0900

    PDFBox 既存のPDFファイルを読み込む

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
     jcenter()
+    maven("https://dl.bintray.com/kotlin/kotlin-eap")
 }
 
 tasks.withType<Test> {
@@ -19,8 +20,17 @@ tasks.withType<Test> {
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.5.2")
     implementation("org.apache.pdfbox:pdfbox:2.0.22")
+    implementation(kotlin("stdlib-jdk8"))
 }
 
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "1.8"
+}
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "1.8"
 }
--- src/main/kotlin/pdfbox/Sample.kt
+++ src/main/kotlin/pdfbox/Sample.kt
@@ -1,4 +1,4 @@
-package pdfbox
+package main.kotlin.pdfbox
 
 import org.apache.pdfbox.pdmodel.PDDocument
 import org.apache.pdfbox.pdmodel.PDPage
@@ -10,28 +10,47 @@ import java.io.ByteArrayOutputStream
 import java.io.File
 
 fun main() {
-    val result = PDDocument().let { doc ->
-        // A4のページを追加
-        val page = PDPage(PDRectangle.A4)
-        doc.addPage(page)
+    val result = createNewPDF()
+    File("output.pdf").writeBytes(result.readBytes())
+    val result2 = openPDF()
+    File("output2.pdf").writeBytes(result2.readBytes())
+}
 
-        // フォントの指定
-        val font = PDType1Font.HELVETICA_BOLD
+private fun openPDF(): ByteArrayInputStream {
+    // resourcesから既存のPDFを読み込む
+    val template = object : Any() {}.javaClass
+        .classLoader
+        .getResourceAsStream("sample.pdf")
 
-        // 指定のページオブジェクトに文字の印字
-        PDPageContentStream(doc, page).use { cs ->
-            cs.beginText()
-            cs.setFont(font, 12f)
-            cs.newLineAtOffset(200f, 500f)
-            cs.showText("Hello World")
-            cs.endText()
-        }
-
-        // ByteArrayInputStreamに変換
+    // ByteArrayOutputStreamに書き込んでByteArrayInputStreamに変換する
+    return PDDocument.load(template).let { doc ->
         val out = ByteArrayOutputStream()
         doc.save(out)
         doc.close()
         ByteArrayInputStream(out.toByteArray())
     }
-    File("output.pdf").writeBytes(result.readBytes())
+}
+
+private fun createNewPDF() = PDDocument().let { doc ->
+    // A4のページを追加
+    val page = PDPage(PDRectangle.A4)
+    doc.addPage(page)
+
+    // フォントの指定
+    val font = PDType1Font.HELVETICA_BOLD
+
+    // 指定のページオブジェクトに文字の印字
+    PDPageContentStream(doc, page).use { cs ->
+        cs.beginText()
+        cs.setFont(font, 12f)
+        cs.newLineAtOffset(200f, 500f)
+        cs.showText("Hello World")
+        cs.endText()
+    }
+
+    // ByteArrayInputStreamに変換
+    val out = ByteArrayOutputStream()
+    doc.save(out)
+    doc.close()
+    ByteArrayInputStream(out.toByteArray())
 }
