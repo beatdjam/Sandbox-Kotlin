2cf456d34c1a50d2cf8238cf59f86fcf8df8c192
T.Hori <beat3526@gmail.com>
2021-04-11 10:55:44 +0900

    PDFBox サンプルコードのコメント整理

--- src/main/kotlin/pdfbox/Sample.kt
+++ src/main/kotlin/pdfbox/Sample.kt
@@ -21,6 +21,7 @@ fun main() {
 private fun openPDF() {
     // resourcesから既存のPDFを読み込む
     val doc = openPDF("sample.pdf")
+    // 指定したファイル名でドキュメントを保存
     File("output.pdf").writeBytes(doc.saveToByteArrayInputStream().readBytes())
 }
 
@@ -29,8 +30,10 @@ private fun openPDF() {
  *
  */
 private fun createNewPDF() {
+    // A4の新規ページを作成する
     val doc = PDDocument().also { it.addPage(PDPage(PDRectangle.A4)) }
     val result = editDocument(doc)
+    // 指定したファイル名でドキュメントを保存
     File("output2.pdf").writeBytes(result.readBytes())
 }
 
@@ -47,6 +50,7 @@ fun editDocument(doc: PDDocument): ByteArrayInputStream {
         doc,
         object : Any() {}.javaClass.classLoader.getResourceAsStream("ipag.ttf")
     )
+
     // とりあえずすべてのページに同じ内容を書き込む
     doc.pages.forEach { page ->
         PDPageContentStream(doc, page).use { cs ->
@@ -55,25 +59,33 @@ fun editDocument(doc: PDDocument): ByteArrayInputStream {
             // 描画領域の中心を算出する
             val tx: Float = (page.mediaBox.lowerLeftX + page.mediaBox.upperRightX) / 2
             val ty: Float = (page.mediaBox.lowerLeftY + page.mediaBox.upperRightY) / 2
+
+            // 逆さで文字を描画
             cs.rotate(180.0, tx, ty)
             // 中心に文字を描画
             cs.writeText("Hello World", font, 12f, tx, ty)
             cs.restoreGraphicsState()
+
             // 描画の向きを戻して再描画
             cs.writeText("Hello World", font, 12f, tx, ty)
+
             // 右寄せで文字列を描画
             cs.writeTextAlignRight("Hello World", font, 12f, tx, ty + 15)
+
             // 中央寄せで文字列を描画
             cs.writeTextAlignCenter("Hello World", font, 12f, tx, 20f)
 
             // 印字文字色変更
             cs.setFontColorByColorCode("#F15B5B")
+            // 折返しのある文字列を描画
             val str =
                 """To obtain the electronic dictionary which pronounces even a long sentence in an easy-to-hear state by preventing the pronunciation from being broken halfway irrelevantly to the meaning as to a long example sentence and a phrase entered into a dictionary and an English equivalent in a Japanese- English dictionary."""
             cs.writeWrapedText(str, font, 12f, 0f, page.mediaBox.height - 10, 300f)
 
+            // 印字文字色を黒に戻す
             cs.setFontColorByColorCode("#000000")
-            // 日本語を描画
+
+            // 日本語を描画(日本語用のフォントを使用)
             cs.writeText("IPA Pゴシック", jpFont, 12f, tx, 40f)
         }
     }
