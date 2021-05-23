36db368497f8e816f0a2ca94b265e268c2cf33f8
T.Hori <beat3526@gmail.com>
2021-05-24 02:51:09 +0900

    通貨の値オブジェクトを実装

--- DDD/src/main/kotlin/2.kt
+++ DDD/src/main/kotlin/2.kt
@@ -1,6 +1,21 @@
 fun main() {
+    // 氏名の値オブジェクト
     val fullName = FullName(FirstName("taro"), LastName("tanaka"))
     println(fullName.lastName)
+
+    // 通貨の値オブジェクト
+    val myMoney = Money(1000.0, "JPY")
+    val allowance = Money(3000.0, "JPY")
+    val result = myMoney.add(allowance)
+    println(result)
+
+    try {
+        val jpy = Money(1000.0, "JPY")
+        val usd = Money(3000.0, "USD")
+        println(jpy.add(usd))
+    } catch (e : IllegalArgumentException) {
+        println(e)
+    }
 }
 
 @JvmInline
@@ -20,3 +35,10 @@ value class LastName(private val value: String){
 }
 
 data class FullName(val firstName: FirstName, val lastName: LastName)
+
+data class Money(val amount :Double, val currency : String) {
+    fun add(arg : Money) : Money {
+        require(this.currency == arg.currency) {"通貨単位が異なります"}
+        return this.copy(amount = this.amount + arg.amount)
+    }
+}
